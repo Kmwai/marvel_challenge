8d7b45ace59caf465d68314a50399c9acd991f4b
Mwai Karimi <mwaikarimi@Mwais-MacBook-Pro.local>
2017-12-15 13:45:42 +0000

    q1 sol

--- marvel.py
+++ marvel.py
@@ -1,10 +1,13 @@
-from collections import Counter, namedtuple
+import collections
+from collections import Counter, namedtuple, OrderedDict
 import csv
 import re
+from io import StringIO
+
 
 DATA = 'marvel-wikia-data.csv'
 
-Character = namedtuple('Character', 'pid name sid align sex appearances year')
+Character = namedtuple('Character', 'pid, name, sid, align, sex, appearances, year')
 
 
 def convert_csv_to_dict(data=DATA):
@@ -12,7 +15,10 @@ def convert_csv_to_dict(data=DATA):
        https://docs.python.org/3.7/library/csv.html#csv.DictReader
        should return a list of OrderedDicts or a list of Character
        namedtuples (see Character namedtuple above')'''
-    pass
+
+    with open(data) as f:
+        for row in csv.DictReader(f):
+            yield Character._make, row
 
 
 data = list(convert_csv_to_dict())
@@ -22,7 +28,7 @@ def most_popular_characters(n=5):
     '''get the most popular character by number of appearances
        accept an argument of n (int) of most popular characters
        to return (leave default of 5)'''
-    pass
+
 
 
 def max_and_min_years_new_characters():
@@ -36,7 +42,11 @@ def max_and_min_years_new_characters():
 def percentage_female():
     '''Get the percentage of female characters, only look at male and female
        for total, ignore the rest, return a percentage rounded to 2 digits'''
-    pass
+    gender = Counter(g.sex for g in data)
+    female = gender['Female']
+    male = gender['Male']
+    f_p = 100 * female / (male + female)
+    return round(f_p)
 
 
 def good_vs_bad(sex):
